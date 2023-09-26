8e2c3b02f1f28be41ef3591ed6984c529174372d
Aditigadakh <aditigadakh4@gmail.com>
2023-09-26 10:41:06 +0530

    first commit

---
+++ dayfourteen/src/org/tnsif/dayfourteen/threadsynchronization/DepositLimitExceedsException.java
@@ -0,0 +1,12 @@
+package org.tnsif.dayfourteen.threadsynchronization;
+
+
+public class DepositLimitExceedsException extends Exception {
+	public DepositLimitExceedsException() {
+		super("Deposit limit exceeded.....");
+	}
+
+	public DepositLimitExceedsException(String message) {
+		super(message);
+	}
+}
